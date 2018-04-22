47cab83df11bc9081ca8a4ecb48bd65df0209c49
Shushan <noname320@Hotmail.com>
2018-04-21 23:00:03 -0400

    Refactored board genmap stuff

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,11 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.swing.JPanel;
 
@@ -10,78 +16,93 @@ public class Board extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private int w = 512;
-	private int h = 512;
+	private final int w = 512;
+	private final int h = 512;
 
 	private Integer sizeX;
 	private Integer sizeY;
 	private Integer nbBombs;
-	private Element[][] map;
+	private ArrayList<ArrayList<Element>> map;
+	private Point selected = new Point(-1, -1);
 
 	public Board(Integer sizeX, Integer sizeY, Integer nbBombs) {
 		this.sizeX = sizeX;
 		this.sizeY = sizeY;
 		this.nbBombs = nbBombs;
-		map = new Element[sizeX][sizeY];
+		map = new ArrayList<ArrayList<Element>>();
 		genMap();
 	}
 
 	public void genMap() {
-		for (int r = 0; r < sizeX; r++) {
-			for (int c = 0; c < sizeY; c++) {
-				map[r][c] = new Element(0);
-			}
-		}
+		// Initializing elements in array
+		Supplier<Element> elSup = () -> new Element(0);
+		Supplier<ArrayList<Element>> elArraySup = () -> Stream.generate(elSup).limit(sizeX).collect(Collectors.toCollection(ArrayList::new));
+		map = Stream.generate(elArraySup).limit(sizeY).collect(Collectors.toCollection(ArrayList::new));
 
 		for (int i = 0; i < nbBombs; i++) {
-			int r1 = (int) Math.floor(Math.random() * sizeX);
-			int c1 = (int) Math.floor(Math.random() * sizeY);
-			map[r1][c1].setNum(-1);
-			// mappy[r1][c1].reveal();
+			int x1 = (int) Math.floor(Math.random() * sizeX);
+			int y1 = (int) Math.floor(Math.random() * sizeY);
+			map.get(x1).get(y1).setNum(-1);
 		}
-
-		for (int r = 0; r < sizeX; r++) {
-			for (int c = 0; c < sizeY; c++) {
-				if (map[r][c].getNum() != -1) {
-					int count = 0;
-					if (r > 0) {
-						if (map[r - 1][c].getNum() == -1)
-							count++;
-						if (c > 0) {
-							if (map[r - 1][c - 1].getNum() == -1)
-								count++;
-						}
-						if (c < sizeY - 1) {
-							if (map[r - 1][c + 1].getNum() == -1)
-								count++;
-						}
+		for (ListIterator<ArrayList<Element>> rowI = map.listIterator(); rowI.hasNext(); ) {
+			ArrayList<Element> row = rowI.next();
+			int y = 0;
+			for (ListIterator<Element> colI = row.listIterator(); colI.hasNext(); y = colI.nextIndex()) {
+				Element el = colI.next();
+				
+				if (el.getNum() != -1) {
+					int num = 0;
+					if (colI.hasPrevious() && row.get(colI.previousIndex()).getNum() == -1
+							|| colI.hasNext() && row.get(colI.nextIndex()).getNum() == -1)
+						num++;
+					if (rowI.hasPrevious()) {
+						ArrayList<Element> prevRow = map.get(rowI.previousIndex());
+						if (prevRow.get(y).getNum() == -1
+								|| colI.hasPrevious() && prevRow.get(colI.previousIndex()).getNum() == -1
+								|| colI.hasNext() && prevRow.get(colI.nextIndex()).getNum() == -1)
+							num++;
 					}
-					if (c > 0) {
-						if (map[r][c - 1].getNum() == -1)
-							count++;
+					if (rowI.hasNext()) {
+						ArrayList<Element> nextRow = map.get(rowI.nextIndex());
+						if (nextRow.get(y).getNum() == -1
+								|| colI.hasPrevious() && nextRow.get(colI.previousIndex()).getNum() == -1
+								|| colI.hasNext() && nextRow.get(colI.nextIndex()).getNum() == -1)
+							num++;
 					}
-					if (r < sizeX - 1) {
-						if (map[r + 1][c].getNum() == -1)
-							count++;
-						if (c < sizeY - 1) {
-							if (map[r + 1][c + 1].getNum() == -1)
-								count++;
-						}
-						if (c > 0) {
-							if (map[r + 1][c - 1].getNum() == -1)
-								count++;
-						}
-					}
-					if (c < sizeY - 1) {
-						if (map[r][c + 1].getNum() == -1)
-							count++;
-					}
-					map[r][c].setNum(count);
+					el.setNum(num);
 				}
 			}
 		}
 	}
 
+	public Boolean select(Integer x, Integer y) {
+		selected = new Point(x, y);
+		if (map.get(x).get(y).getNum() == -1) {
+			return false;
+		}
+		reveal(x, y);
+		return true;
+	}
+	
+	private void reveal(Integer x, Integer y) {
+		
+	}
+	
+	public Boolean check() {
+		int count = 0;
+		for (ArrayList<Element> row : map) {
+			for (Element el : row) {
+				if (el.getHidden())
+					count++;
+			}
+		}
+		return count == nbBombs;
+	}
+	
+	public ArrayList<ArrayList<Element>> getMap() {
+		return map;
+	}
+	
 	public void paintComponent(Graphics g) {
 		setOpaque(true);
 		super.paintComponent(g);
@@ -102,9 +123,9 @@ public class Board extends JPanel {
 		}
 
 		for (int r = 0; r < sizeX; r++) {
-			Element[] row = map[r];
+			ArrayList<Element> row = map.get(r);
 			for (int c = 0; c < sizeY; c++) {
-				Element e = row[c];
+				Element e = row.get(c);
 				if (!e.getHidden()) {
 					if (e.getNum() == 0) {
 						g.setColor(Color.DARK_GRAY);
@@ -115,7 +136,7 @@ public class Board extends JPanel {
 					} else if (e.getNum() == 3) {
 						g.setColor(Color.RED);
 					} else {
-						g.setColor(Color.PINK);
+						g.setColor(Color.BLACK);
 					}
 					int x = c * w / sizeX + w / (2 * sizeX) - 2;
 					int y = (r + 1) * h / sizeY - h / (2 * sizeY) + 5;
@@ -130,17 +151,13 @@ public class Board extends JPanel {
 
 			}
 		}
-		//
-		// g.setColor(Color.YELLOW);
-		// g.drawLine(a[0] * w / s, a[1] * h / s, a[0] * w / s, (a[1] + 1) * h /
-		// s);
-		// g.drawLine((a[0] + 1) * w / s, a[1] * h / s, (a[0] + 1) * w / s,
-		// (a[1] + 1) * h / s);
-		// g.drawLine(a[0] * w / s, a[1] * h / s, (a[0] + 1) * w / s, a[1] * h /
-		// s);
-		// g.drawLine(a[0] * w / s, (a[1] + 1) * h / s, (a[0] + 1) * w / s,
-		// (a[1] + 1) * h / s);
 
+		g.setColor(Color.YELLOW);
+		g.drawLine(selected.x * w/sizeX, selected.y * h/sizeY, selected.x* w/sizeX, (selected.y+1) * h/sizeY);
+		g.drawLine((selected.x+1)* w/sizeX, selected.y* h/sizeY, (selected.x+1)* w/sizeX, (selected.y+1) * h/sizeY);
+		g.drawLine(selected.x* w/sizeX, selected.y* h/sizeY, (selected.x+1)* w/sizeX, selected.y* h/sizeY);
+		g.drawLine(selected.x* w/sizeX, (selected.y+1) * h/sizeY, (selected.x+1)* w/sizeX, (selected.y+1) * h/sizeY);
+		
 		repaint();
 	}
 }
